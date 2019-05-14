2a6dd31155f1b10660139ac4954f1b5ffc4acbb9
Olof Hultberg <olof.hultberg@Olofs-MacBook-Pro.local>
2019-05-14 20:57:55 +0200

    Add firebase to project

--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from '@/store'
+import firebase from 'firebase'
 import AppDate from '@/components/AppDate'
 
 Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA34yoyvTnkSOl9aOUvXdEN5O2ij4t7IDM',
+  authDomain: 'vue-shool-forum.firebaseapp.com',
+  databaseURL: 'https://vue-shool-forum.firebaseio.com',
+  projectId: 'vue-shool-forum',
+  storageBucket: 'vue-shool-forum.appspot.com',
+  messagingSenderId: '415999539264',
+  appId: '1:415999539264:web:6f1cf3ca68e0f37b'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
